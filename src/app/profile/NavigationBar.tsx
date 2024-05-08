d002c74ee09e2e625a1ac1b1e451be94745c77fb
Uchenna W <uchennanwilliams@gmail.com>
2024-05-08 18:04:11 -0400

    nav bar

--- src/app/profile/NavigationBar.tsx
+++ src/app/profile/NavigationBar.tsx
@@ -1,13 +1,81 @@
+"use client";
 
+import {Icon} from "../profile/page"
+import Image from "next/image"
+import styled from "styled-components"
+
+
+const Navigation = styled.aside`
+        display: grid;
+        grid-template-rows: repeat(6, min-content);
+        width: fit-content;
+        position: fixed;
+        height: 100%;
+        place-content: center;
+        grid-gap: 10px;
+        border-right: 1px solid rgba(255, 255, 255, 0.1);
+        padding: 0px 10px;
+
+        button {
+            background-color: transparent;
+            border: none;
+            width: 48px;
+            height: 48px;
+            transition: all 0.2s ease;
+            &:hover{
+                background-color: #222222;
+                border-radius: 9px;
+                transform: scale(1.1);
+            }
+        }
+
+        //tablet
+        @media (max-width: 768px) {
+            border-top: 1px solid rgba(255, 255, 255, 0.1);
+            display: grid;
+            grid-template-columns: repeat(6, min-content);
+            grid-template-rows: 1fr;
+            height: fit-content;
+            bottom: 0px;
+            width: 100%;
+            padding: 10px 0px;
+            z-index: 10;
+
+        }
+
+        //mobile phone
+        @media (max-width: 480px) {
+            height: 73px;
+            bottom: 0px;
+        }
+`;
+
+
+interface NavigationBarProps {
+    items: Icon[]
+}
 
 //recieve array of icons
-export default function NavigationBar(){
+export default function NavigationBar({items}: NavigationBarProps){
+    // items.map((icon) => {
+    //     console.log(icon.altText);
+    // })
+
     return (
         <>
-        <p>This is the nav compo</p>
+        
          {/* {loop} */}
          {/* {button} */}
          {/* {image} */}
+         <Navigation>
+         {items.map((icon, index) => {
+            return (
+                <button key={index}>
+                <Image src={icon.path} width={icon.width} height={icon.height} alt={icon.altText} />
+            </button>
+            );
+         })}
+         </Navigation>
         </>
     )
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,19 +1,39 @@
 import NavigationBar from "./NavigationBar";
 
 //import icons
-import CompassIcon from "../../../public/icon/compasss.svg"
-import FilmIcon from "../../../public/icon/film.svg"
-import HeartIcon from "../../../public/icon/heart.svg"
-import HouseIcon from "../../../public/icon/house.svg"
-import MagnifyingGlassIcon from "../../../public/icon/magnifyingglass.svg"
-import MessageIcon from "../../../public/icon/message.svg"
+import CompassIcon from "../../../public/icons/compass.svg"
+import FilmIcon from "../../../public/icons/film.svg"
+import HeartIcon from "../../../public/icons/heart.svg"
+import HouseIcon from "../../../public/icons/house.svg"
+import MagnifyingGlassIcon from "../../../public/icons/magnifyingglass.svg"
+import MessageIcon from "../../../public/icons/message.svg"
 
 //create array of icons to pass to navbar 
+
+export interface Icon {
+    path: string;
+    altText: string;
+    width: number;
+    height: number;
+}
+const iconHeight = 25;
+const iconWidth = 25;
+
+const navbarConfigItems: Icon[] = [
+    {path: CompassIcon, altText: "Discover", width: iconWidth, height: iconHeight},
+    {path: FilmIcon, altText: "Reels", width: iconWidth, height: iconHeight},
+    {path: HeartIcon, altText: "Like", width: iconWidth, height: iconHeight},
+    {path: HouseIcon, altText: "Home", width: iconWidth, height: iconHeight},
+    {path: MagnifyingGlassIcon, altText: "Search", width: iconWidth, height: iconHeight},
+    {path: MessageIcon, altText: "Message", width: iconWidth, height: iconHeight}
+
+
+]
 export default function Profile() {
     return (
         <>
         {/* Nav bar*/}
-        <NavigationBar/>
+        <NavigationBar items={navbarConfigItems}/>
         {/* Header */}
         {/* photogrid */}
         {/* stories */}
